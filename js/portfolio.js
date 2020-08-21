effa0ed6eed89056a2b556179742183766775c1f
vahe54262 <70031451+vahe54262@users.noreply.github.com>
2020-08-21 21:49:27 +0400

    Add files via upload

---
+++ js/portfolio.js
@@ -0,0 +1,9 @@
+new WOW().init();
+var mySwiper = new Swiper('.swiper-container', {
+  pagination: {
+    el: '.projects-pagination',
+    bulletClass:'projects_bullet',
+    bulletActiveClass:'projects_bullet-active',
+    clickable:true
+  },
+});
